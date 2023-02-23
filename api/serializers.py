5a1403e2c0aedcd9e5048fc1f951d7183e258981
AzizJohn <azizjon1708@gmail.com>
2023-02-24 00:13:24 +0500

    commitment

--- api/serializers.py
+++ api/serializers.py
@@ -7,7 +7,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
-
+#nhckhcguls
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
